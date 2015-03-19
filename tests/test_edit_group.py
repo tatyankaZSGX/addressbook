5e85ed753251731e862a0636d7929ce853bdf44f
Tatyana <kozlovitcht@gmai.com>
2015-03-19 22:37:23 +0300

    предусловия для групп

--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -2,4 +2,6 @@ __author__ = 'ZSGX'
 from model.group import Group
 
 def test_edit_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name = "test"))
     app.group.edit_first_group(Group(name="new_name"))
