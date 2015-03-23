970f1f78d014ff1e23a96008252a591a37048cf5
Tatyana <kozlovitcht@gmai.com>
2015-03-23 20:54:15 +0300

    кеширование списка групп

--- fixture/group.py
+++ fixture/group.py
@@ -27,6 +27,7 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def return_to_groups_page(self):
         wd = self.app.wd
@@ -42,6 +43,7 @@ class GroupHelper:
         self.select_first_group()
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def edit_first_group(self, Group):
         wd = self.app.wd
@@ -53,19 +55,23 @@ class GroupHelper:
         #submit editing
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def count(self):
         wd = self.app.wd
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    group_cache = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        gr_list = []
-        for el in wd.find_elements_by_css_selector("span.group"):
-            text = el.text
-            id = el.find_element_by_name("selected[]").get_attribute("value")
-            gr_list.append(Group(name=text, id=id))
-        return gr_list
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.group_cache = []
+            for el in wd.find_elements_by_css_selector("span.group"):
+                text = el.text
+                id = el.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
 
