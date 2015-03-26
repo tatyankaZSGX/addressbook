673f139f67444c7364db1884ea1ec22bdfe92623
Tatyana <kozlovitcht@gmai.com>
2015-03-26 19:49:48 +0200

    Удаление/модификация случайной группы

--- tests/test_del_group.py
+++ tests/test_del_group.py
@@ -1,5 +1,6 @@
 __author__ = 'ZSGX'
 from model.group import Group
+from random import randrange
 
 def test_delete_first_group(app):
     if app.group.count() == 0 :
@@ -8,5 +9,17 @@ def test_delete_first_group(app):
     app.group.delete_first_group()
     assert len(old_groups) - 1 == app.group.count()
     old_groups[0:1]=[]
+    new_groups = app.group.get_group_list()
+    assert old_groups == new_groups
+
+
+def test_delete_rand_group(app):
+    if app.group.count() == 0 :
+        app.group.create(Group(name = "test"))
+    old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
+    app.group.delete_group_by_index(index)
+    assert len(old_groups) - 1 == app.group.count()
+    old_groups[index:index+1]=[]
     new_groups = app.group.get_group_list()
     assert old_groups == new_groups
--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -1,5 +1,6 @@
 __author__ = 'ZSGX'
 from model.group import Group
+from random import randrange
 
 def test_edit_first_group(app):
     if app.group.count() == 0:
@@ -11,4 +12,18 @@ def test_edit_first_group(app):
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
+def test_edit_rand_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name = "test"))
+    old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
+    group = Group(name="random group")
+    group.id = old_groups[index].id
+    app.group.edit_group_by_index(index, group)
+    assert len(old_groups) == app.group.count()
+    new_groups = app.group.get_group_list()
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
