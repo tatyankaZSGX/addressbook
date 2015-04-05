d6f201658973384f29d19ce481a8a94f1a4fe674
Tatyana <kozlovitcht@gmai.com>
2015-04-05 17:55:06 +0200

    browser/url options

--- conftest.py
+++ conftest.py
@@ -6,13 +6,15 @@ from fixture.application import Application
 fixture = None
 
 @pytest.fixture()
-def app():
+def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    homeurl = request.config.getoption("--homeurl")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser, homeurl)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser, homeurl)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -23,3 +25,7 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--homeurl" ,action="store", default="http://localhost/addressbook/")
--- fixture/application.py
+++ fixture/application.py
@@ -1,17 +1,25 @@
 __author__ = 'ZSGX'
 
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.Session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, homeurl):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Browser %s is not recognized" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.homeurl = homeurl
 
     def is_valid(self):
         try:
@@ -22,7 +30,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.homeurl)
 
     def destroy(self):
         self.wd.quit()
